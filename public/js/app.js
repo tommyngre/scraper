4dfd5329b970f8756b58addb69987d21970bde7b
unknown <tommyn.gre@gmail.com>
2018-07-01 21:54:46 -0400

    + little refinements

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 //render form when "Add note" clicked
-$('.add-note').on('click', function(){
+$('.add-note').on('click', function () {
   let articleId = $(this).attr('data-id');
 
   //remove any comment forms before opening a new one
   $('.new-comment').remove();
 
   let comment = $(`<div class="new-comment" data-articleId=${articleId}>`);
-  let html=
+  let html =
     `<form>
   <div class="form-group">
     <input type="text" class="form-control" id="comment-title" placeholder="Enter a title for your comment" data-articleId=${articleId}>
@@ -16,15 +16,15 @@ $('.add-note').on('click', function(){
   </div>
   <div class="btn btn-primary btn-sm save-note" data-articleId=${articleId}><i class="fas fa-save"></i> Save comment</div>
   </form>`;
-  
+
   comment.html(html);
 
   $(`.comments-wrapper[data-id=${articleId}]`)
-  .prepend(comment);
+    .prepend(comment);
 });
 
 //save comment when "Save comment" clicked
-$(document).on("click", ".save-note", function() {
+$(document).on("click", ".save-note", function () {
   var articleId = $(this).attr("data-articleId");
 
   //POST form elems
@@ -36,8 +36,9 @@ $(document).on("click", ".save-note", function() {
       comment: $("#comment").val()
     }
   })
-    .then(function(data) {
-      console.log(data);
+    .then(function (data) {
+      //console.log(data);
+      location.reload();
     });
 
   //remove vals from form, remove form
@@ -47,36 +48,36 @@ $(document).on("click", ".save-note", function() {
 });
 
 //unlock comment for editing
-$(document).on('click','.unlock-note, .save-old-note', function(){
+$(document).on('click', '.unlock-note, .save-old-note', function () {
   let id = $(this).attr('data-noteid');
   //if fields readOnly, make editable, vice verse
   if ($(`input[data-noteid=${id}]`).attr('readOnly')) {
-    $(`input[data-noteid=${id}]`).attr('readOnly',false);
-    $(`textarea[data-noteid=${id}]`).attr('readOnly',false);
+    $(`input[data-noteid=${id}]`).attr('readOnly', false);
+    $(`textarea[data-noteid=${id}]`).attr('readOnly', false);
     //swap unlock button w save button
     $(`.unlock-note[data-noteid=${id}]`).hide();
     $(`.save-old-note[data-noteid=${id}]`).show();
   } else {
-    $(`input[data-noteid=${id}]`).attr('readOnly',true);
-    $(`textarea[data-noteid=${id}]`).attr('readOnly',true);  
+    $(`input[data-noteid=${id}]`).attr('readOnly', true);
+    $(`textarea[data-noteid=${id}]`).attr('readOnly', true);
     //swap unlock button w save button
     $(`.unlock-note[data-noteid=${id}]`).show();
     $(`.save-old-note[data-noteid=${id}]`).hide();
   }
-  
+
 });
 
 //save comment when "Save comment" clicked
-$(document).on("click", ".save-old-note", function() {
+$(document).on("click", ".save-old-note", function () {
   var noteId = $(this).attr("data-noteId");
 
   let update = {
     _id: noteId,
     title: $(`input[data-noteid=${noteId}]`).val(),
-    comment: $(`textarea[data-noteid=${noteId}]`).val() 
+    comment: $(`textarea[data-noteid=${noteId}]`).val()
   }
 
-  console.log("PUT",update);
+  console.log("PUT", update);
 
   $.ajax("/notes/" + noteId,
     {
@@ -105,3 +106,22 @@ $(document).on('click', '.delete-note', function (event) {
     })
 
 });
+
+function scrape(){
+  $.ajax({
+    method: "GET",
+    url: "/scrape",
+  })
+    .then(function () {
+      location.reload();
+    });
+}
+
+$(document).ready(function () {
+  //TODO
+  //scrape on load
+  //-duplicate-check
+  //-load new to old
+  
+  //scrape();
+})
